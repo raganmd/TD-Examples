8b53628d7853368c7c79937e98a3e7da28e27e4e
M Ragan <raganmd@gmail.com>
2016-05-01 14:26:48 -0700

    Updates to for loop examples

--- ragan/python_in_touchdesigner/example_for_loop/text_create_ops_loop.py
+++ ragan/python_in_touchdesigner/example_for_loop/text_create_ops_loop.py
@@ -5,16 +5,13 @@ new_ops_list = [
 	'text_newop3'
 	]
 
-node_distance = 200
-
-# create an enumerated list from the original
-new_ops_enumerate = list( enumerate( new_ops_list) )
+node_distance = 100
 
 # loop through list
-for item in new_ops_enumerate:
+for item in enumerate( new_ops_list ):
 	# create and name op 
 	new_op = parent().create( textDAT , item[ 1 ] )
 
 	# set location of nodes ( x or y )
-	#new_op.nodeX = item[ 0 ] * node_distance
+	new_op.nodeX = me.nodeX
 	new_op.nodeY = - ( item[ 0 ] * node_distance )

2957dacc1856cbe86b4287370d88fc44959e8057
Matthew Ragan <matthew.ragan@obscuradigital.com>
2016-05-06 10:23:58 -0700

    Updates to for loop example Updates to creating ops with loops

--- ragan/python_in_touchdesigner/example_for_loop/text_create_ops_loop.py
+++ ragan/python_in_touchdesigner/example_for_loop/text_create_ops_loop.py
@@ -1,3 +1,17 @@
+# we can also use loops to do all sorts of exciting things
+# like creat and place operators.
+
+# What we'll quikcly realize here is that for loops and
+# the replicator COMP look very similar in the way they
+# operate when it comes to creating ops!
+
+# You might feel like the replicator is good enough, so why
+# learn how this work?! Sometimes knowing exactly how a process
+# works can help us better understand another process. 
+
+# In this case, setting up our own replicator script can 
+# teach us a lot about the replicator. 
+
 # set some variables
 new_ops_list = [
 	'text_newop1' ,
@@ -5,6 +19,7 @@ new_ops_list = [
 	'text_newop3'
 	]
 
+# We'll use this to determine the space between operators.
 node_distance = 100
 
 # loop through list

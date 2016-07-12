7c7bb49d4ac33d3d821f96edab7e206561355198
Matthew Ragan <matthew.ragan@obscuradigital.com>
2016-07-12 13:51:55 -0700

    Added modules examples

--- ragan/python_in_touchdesigner/example_modules/text_module1.py
+++ ragan/python_in_touchdesigner/example_modules/text_module1.py
@@ -36,6 +36,22 @@ log_message			= '''----------------------------
 '''
 
 def Full_date():
+	'''Create a formatted time stamp
+
+	A look at how we might create a formatted time stamp to use with
+	various logging applications.
+	
+	Arguments
+	---------------	
+	None
+
+	Returns
+	---------------
+	formatted_text( str ) - a string time stamp
+
+	Notes
+	---------------
+	'''
 
 	now			= datetime.datetime.now()
 	year		= datetime.datetime.now().year
@@ -59,7 +75,33 @@ def Full_date():
 	return formatted_text
 
 def Log_message( operator, message, verbose=False, text_port_print=True, append_log=True ):
+	'''Logging Method.
+
+	A simple look at how you might start to think about building a logger for a TouchDesigner
+	application. A logger is a great way to build out files with time stamped events. The
+	more complex a project becomes, the more important it can become to have some means
+	of logging the operations of your program. Here's a simple look at what that might
+	look like.
 	
+	Arguments
+	---------------	
+	operator( touch object ) - the touch object whose path you'd like incldued in the log message
+	message( str ) - a message to include in the log
+	verbose( bool ) - a toggle for verbose or compact messages
+	text_port_print( bool ) - a toggle to print to the text port, or not
+	append_log( bool ) - a toggle to append to the log file , or not
+
+	Returns
+	---------------
+	None
+
+	Notes
+	---------------
+	You'll notice that some arguments receive default values. This is so you don't have
+	to include them in the call. This means that by default the message will be compact, 
+	will print to the text port, and will append the log file.
+	'''
+
 	now			= datetime.datetime.now()
 	year		= datetime.datetime.now().year
 	month		= datetime.datetime.now().month
